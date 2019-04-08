c5de01f6287136463076ab2ee33c170700fd853a
David J. Neu <david@davidneu.com>
2019-04-07 21:13:53 -0400

    Added touch and testit to tools.clj.

--- src/mvt_clj/tools.clj
+++ src/mvt_clj/tools.clj
@@ -1,10 +1,29 @@
 (ns mvt-clj.tools
   (:require
-  [clojure.tools.namespace.repl]      
-   [mvt-clj.error :refer [print-error]]))
+   [clojure.tools.namespace.repl :as ctnr]
+   [clojure.java.io :as io]
+   [java-time :as jt]
+   [eftest.runner :as eftest]
+   [mvt-clj.error]))
+
+;; (clojure.tools.namespace.repl/disable-unload!)
 
 (defn refresh [& options]
   (let [r (apply clojure.tools.namespace.repl/refresh options)]
     (if (instance? Exception r)
-      (print-error r)
+      (mvt-clj.error/print-error r)
       r)))
+
+(defn testit []
+  (binding [clojure.test/*test-out* *out*]
+    (eftest/run-tests (eftest/find-tests "test"))))
+
+(defn touch [fs]
+  (doseq [f fs]
+    (let [af (clojure.java.io/file f)]
+      (if (.exists af)
+        (.setLastModified
+         af
+         (jt/to-millis-from-epoch (jt/instant)))
+        (throw (Exception. (format "File %s not found" (.getCanonicalPath af))))))))
+
