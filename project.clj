fafe5e466be94d5cd1f4614ee935052730689b24
David J. Neu <david@davidneu.com>
2019-04-09 11:12:03 -0400

    Clean up deps.edn and project.clj.

--- project.clj
+++ project.clj
@@ -3,10 +3,12 @@
   :url "http://github.com/davidneu/mvt-clj"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1-beta1"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [clojure-complete "0.2.5"]
-                 [prone "1.6.1"]]
+                 [prone "1.6.1"]
+                 [clojure.java-time "0.3.2"]
+                 [eftest "0.5.7"]]
   :local-repo ".m2"
   :jvm-opts ["-Dclojure.server.repl={:port 5555 :accept mvt-clj.repl/socket-repl}"
              "-Xms2g"
